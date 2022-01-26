07970784aac24e46129396ee30144128dad822f1
Greg <greg_markarian@hotmail.com>
2022-01-26 10:15:54 +0200

    update

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,18 +1,25 @@
-import { Tab, Tabs } from "@mui/material";
+import { AdbOutlined } from "@material-ui/icons";
+import { Tab, Tabs, AppBar } from "@mui/material";
 import React from "react";
+import Contact from "./Contact";
+import About from "../src/About"
 
 const HomePage = (props) => {
-  const [selectTab, setselectTab] = React.useState(0);
+  const [selectedTab, setselectedTab] = React.useState(0);
 
   const handleChange = (event, newValue) => {
-    setselectTab(newValue);
+    setselectedTab(newValue);
   };
   return (
     <div>
-      <Tabs value={selectTab} onChange={handleChange}>
+      {/* <AppBar position="static"> */}
+      <Tabs value={selectedTab} onChange={handleChange}>
         <Tab label='Item One' />
         <Tab label='Item Two' />
       </Tabs>
+      {/* </AppBar> */}
+      {selectedTab === 0 && <About />}
+      {selectedTab === 1 && <Contact />}
     </div>
   );
 };
